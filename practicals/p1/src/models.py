23b42188c569055bafdd46c1d665571d782851ec
brunosangom <brunosangom@gmail.com>
2025-03-06 23:47:17 +0100

    added models.py

--- practicals/p1/src/models.py
+++ practicals/p1/src/models.py
@@ -8,7 +8,8 @@ from keras.layers import (
     MultiHeadAttention,
     LayerNormalization,
     Average,
-    Dropout
+    Dropout,
+    Layer,
 )
 from tensorflow.keras.models import Model
 
@@ -16,6 +17,14 @@ tf.config.run_functions_eagerly(True)
 from config import *
 from experiment_config import ExperimentConfig
 import tensorflow.keras.applications as app
+
+class ExpandDims(Layer):
+    def call(self, x):
+        return tf.expand_dims(x, axis=1)
+
+class Squeeze(Layer):
+    def call(self, x):
+        return tf.squeeze(x, axis=1)
 
 
 def setup_best_config(
@@ -116,32 +125,33 @@ def add_classifier_head(
         predictions3 = Dense(num_classes, activation=exp.last_layer_activation)(x3)
 
         # Average the outputs
-        ensemble_output = Average()([predictions1, predictions2, predictions3])
-
-        # Create the model
-        model = Model(inputs=base_model.input, outputs=ensemble_output)
+        predictions = Average()([predictions1, predictions2, predictions3])
         
     elif exp.classifier_head == "attention":
         # Transformer Head
         embed_dim = 256   # Embedding dimension
         num_heads = 4     # Number of attention heads
-        ff_dim = 1024     # Feedforward layer dimension
+        ff_dim = 512      # Feedforward layer dimension
         num_layers = 1    # Number of Transformer encoder layers
 
+        x = Dense(embed_dim, activation="relu")(x)
+
         # Expand feature vector to sequence (1, embed_dim) to match Transformer input format
-        x = tf.expand_dims(x, axis=1)
+        x = ExpandDims()(x)
 
         # Transformer Encoder Layer
         for _ in range(num_layers):
-            attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)(x, x)
+            attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim // num_heads)(x, x)
             attn_output = LayerNormalization()(attn_output + x)
             ff_output = Dense(ff_dim, activation="relu")(attn_output)
             ff_output = Dense(embed_dim)(ff_output)
+            ff_output = Dropout(0.2)(ff_output)
             x = LayerNormalization()(ff_output + attn_output)
-
+        
         # Remove sequence dimension and project to output classes
-        x = tf.squeeze(x, axis=1)
+        x = Squeeze()(x)
         predictions = Dense(num_classes, activation=exp.last_layer_activation)(x)
+        
         
     else:
         raise ValueError("Invalid classifier head type")
