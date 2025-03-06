4c210b3a40cb845500bf1d49e4330cbbff21b248
SheenaGit <91433803+SheenaGit@users.noreply.github.com>
2025-03-06 11:43:44 +0100

    Added mAP metric to train and evaluate flow

--- practicals/p1/src/metrics.py
+++ practicals/p1/src/metrics.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from keras import backend as K
 
-
 def recall_m(y_true, y_pred):
     y_true = tf.cast(y_true, tf.float32)
     y_pred = tf.cast(y_pred, tf.float32)
@@ -24,3 +23,21 @@ def f1_metric(y_true, y_pred):
     precision = precision_m(y_true, y_pred)
     recall = recall_m(y_true, y_pred)
     return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
+def average_precision(y_true, y_pred):
+    sorted_indices = tf.argsort(y_pred, axis=-1, direction='DESCENDING') 
+    sorted_true = tf.gather(y_true, sorted_indices, batch_dims=1) 
+
+    cumulative_true = tf.cumsum(sorted_true, axis=-1)
+    cumulative_total = tf.range(1, tf.shape(sorted_true)[-1] + 1, dtype=tf.float32)  
+
+    precision_at_k = cumulative_true / cumulative_total
+    AP = tf.reduce_sum(precision_at_k * sorted_true, axis=-1) / (tf.reduce_sum(sorted_true, axis=-1) + K.epsilon())
+
+    return AP
+
+def mean_average_precision(y_true, y_pred):
+    AP_per_class = average_precision(y_true, y_pred)
+    return tf.reduce_mean(AP_per_class)  
+
+
