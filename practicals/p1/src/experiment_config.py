bc3894e5eee415da59a2a28d51ef13f62890dd7f
brunosangom <brunosangom@gmail.com>
2025-03-06 23:48:09 +0100

    - implemented models.py to main.ipynb - small name changes in experiment_config.py for consistency

--- practicals/p1/src/experiment_config.py
+++ practicals/p1/src/experiment_config.py
@@ -93,8 +93,8 @@ experiments = {
             itertools.product([16, 32, 64], [0.001, 0.01, 0.1])
         )
     ],
-    # "augmentation_experiments": [], # Add to the id of classifier_head_experiments the number of experiments performed here
-    "classfier_head_experiments": [
+    "augmentation-experiments": [], # Add to the id of classifier_head_experiments the number of experiments performed here
+    "classfier_head-experiments": [
         {
             "id": 19 + i,
             "title": f"classifier_head: {classifier_head}",
