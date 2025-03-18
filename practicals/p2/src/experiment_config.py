5daf8dc6a2c0e5eed931483aa6abb900c3ed4626
Zach Parent <zachparent@duck.com>
2025-03-18 14:08:52 +0000

    update requirements.txt

--- practicals/p2/src/experiment_config.py
+++ practicals/p2/src/experiment_config.py
@@ -1,5 +1,6 @@
 import pydantic
 
+
 class ExperimentConfig(pydantic.BaseModel):
     id: int
     model_name: str
@@ -7,16 +8,22 @@ class ExperimentConfig(pydantic.BaseModel):
     batch_size: int
     epochs: int = 4
 
+
 class ExperimentSet(pydantic.BaseModel):
     title: str
     configs: list[ExperimentConfig]
 
+
 hyperparameter_search = ExperimentSet(
     title="Hyperparameter Search",
     configs=[
-        ExperimentConfig(id=0, model_name="resnet18", learning_rate=0.001, batch_size=32),
-        ExperimentConfig(id=1, model_name="resnet18", learning_rate=0.0001, batch_size=32),
+        ExperimentConfig(
+            id=0, model_name="resnet18", learning_rate=0.001, batch_size=32
+        ),
+        ExperimentConfig(
+            id=1, model_name="resnet18", learning_rate=0.0001, batch_size=32
+        ),
     ],
 )
 
-experiments = [hyperparameter_search]
+EXPERIMENT_SETS = [hyperparameter_search]
--- practicals/p2/src/main.py
+++ practicals/p2/src/main.py
@@ -1,5 +1,5 @@
 from config import RAW_DATA_DIR
-from experiment_config import ExperimentConfig, experiments
+from experiment_config import ExperimentConfig, EXPERIMENT_SETS
 from dataset import get_dataloaders, load_category_mappings
 from pathlib import Path
 from config import TRAIN_ANNOTATIONS_JSON
@@ -7,9 +7,10 @@ from visualize import visualize_segmentation
 from run_experiment import run_experiment
 from pprint import pprint
 
+
 def main():
 
-    for experiment_set in experiments:
+    for experiment_set in EXPERIMENT_SETS:
         print("==================================================")
         print(f"\tRunning experiment set: {experiment_set.title}")
         print("==================================================")
@@ -18,6 +19,7 @@ def main():
             pprint(experiment)
             print("==================================================")
             run_experiment(experiment)
+
 
 if __name__ == "__main__":
     main()
