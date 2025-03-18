2dbdd13bb1c9e57b4efa1d14a4498c69a126ff8a
Zach Parent <zachparent@duck.com>
2025-03-18 16:26:29 +0000

    format

--- practicals/p2/src/dataset.py
+++ practicals/p2/src/dataset.py
@@ -212,7 +212,6 @@ class FashionpediaSegmentationDataset(Dataset):
         # Setup resize transform
         self.resize_transform = ResizeTransform(img_size)
 
-
     def __len__(self):
         return len(self.img_ids)
 
--- practicals/p2/src/models.py
+++ practicals/p2/src/models.py
@@ -1,10 +1,10 @@
-
 from torch import nn
 from torchvision import models
 from typing import Literal
 
 
 type ModelName = Literal["resnet18", "resnet50"]
+
 
 def get_model(model_name: ModelName):
     if model_name == "resnet18":
@@ -13,6 +13,6 @@ def get_model(model_name: ModelName):
         model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
     else:
         raise ValueError(f"Model {model_name} not found")
-    
+
     model.fc = nn.Linear(model.fc.in_features, 1000)
     return model
