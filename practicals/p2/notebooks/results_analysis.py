3c32b87ede20d4400db0497c5192085f336df08c
Zach Parent <zachparent@duck.com>
2025-03-28 12:39:21 +0100

    load confusion_matrix differently

--- practicals/p2/notebooks/results_analysis.py
+++ practicals/p2/notebooks/results_analysis.py
@@ -7,20 +7,24 @@ import sys
 sys.path.append("..")
 sys.path.append("../src")
 
-from src.metrics import METRICS_DIR, CONFUSION_MATRICES_DIR
+from src.config import METRICS_DIR, DATA_DIR
 from src.dataset import MAIN_ITEM_NAMES
+
+CONFUSION_MATRICES_DIR = DATA_DIR / "03_confusion_matrices"
 # %%
 labels = ["background"] + MAIN_ITEM_NAMES
-metrics_dfs = [
-    pd.read_csv(path, index_col=0) for path in METRICS_DIR.glob("experiment_*.csv")
-]
-confusion_matrix_dfs = [
-    pd.read_csv(path, index_col=0) for path in CONFUSION_MATRICES_DIR.glob("experiment_*.csv")
-]
+metrics_dfs = {}
+for path in METRICS_DIR.glob("experiment_*.csv"):
+    metrics_dfs[path.stem] = pd.read_csv(path, index_col=0)
+
+confusion_matrix_dfs = {}
+for path in CONFUSION_MATRICES_DIR.glob("experiment_*.csv"):
+    confusion_matrix_dfs[path.stem] = pd.read_csv(path, index_col=0)
+
 
 # %%
 
-cm = confusion_matrix_dfs[0]
+cm = confusion_matrix_dfs["experiment_11"]
 cm = cm.div(cm.sum(axis=1), axis=0)
 # %%
 fig, ax = plt.subplots(1,1, figsize=(12,10))
