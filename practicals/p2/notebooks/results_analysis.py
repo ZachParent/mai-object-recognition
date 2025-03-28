e9b0f0829cd49f91ec2ba58d9cd7fd58aa614197
Zach Parent <zachparent@duck.com>
2025-03-28 12:54:54 +0100

    add a TP-sorted cm

--- practicals/p2/notebooks/results_analysis.py
+++ practicals/p2/notebooks/results_analysis.py
@@ -1,4 +1,5 @@
 # %%
+# imports
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,26 +13,50 @@ from src.dataset import MAIN_ITEM_NAMES
 
 CONFUSION_MATRICES_DIR = DATA_DIR / "03_confusion_matrices"
 # %%
+# load data
 labels = ["background"] + MAIN_ITEM_NAMES
-metrics_dfs = {}
+metrics_dfs: dict[str, pd.DataFrame] = {}
 for path in METRICS_DIR.glob("experiment_*.csv"):
     metrics_dfs[path.stem] = pd.read_csv(path, index_col=0)
 
-confusion_matrix_dfs = {}
+confusion_matrix_dfs: dict[str, pd.DataFrame] = {}
 for path in CONFUSION_MATRICES_DIR.glob("experiment_*.csv"):
     confusion_matrix_dfs[path.stem] = pd.read_csv(path, index_col=0)
 
 
 # %%
+# compute confusion matrix
 
 cm = confusion_matrix_dfs["experiment_11"]
-cm = cm.div(cm.sum(axis=1), axis=0)
-# %%
+# normalize the predictions by the true counts
+true_counts = cm.sum(axis=1)
+cm = cm.div(true_counts, axis=0)
+
+# sort the labels by the true counts
+sorted_indices = true_counts.sort_values(ascending=False).index
+cm = cm.iloc[sorted_indices, sorted_indices]
+sorted_labels = [labels[i] for i in sorted_indices]
+
+# %% 
+# plot confusion matrix
 fig, ax = plt.subplots(1,1, figsize=(12,10))
-logarithmic_cm = np.log(cm + 1)
-sns.heatmap(cm, cmap="Blues", fmt="d", ax=ax, xticklabels=labels, yticklabels=labels, linewidths=0.5, linecolor="lightgrey")
+sns.heatmap(cm, cmap="Blues", fmt="d", ax=ax, xticklabels=sorted_labels, yticklabels=sorted_labels, linewidths=0.5, linecolor="lightgrey")
 ax.set_title("Confusion Matrix")
 ax.set_xlabel("Predicted")
 ax.set_ylabel("True")
 plt.show()
 
+# %% 
+# compute tp-sorted confusion matrix
+tp_counts = cm.values.diagonal()
+tp_sorted_indices = tp_counts.argsort()[::-1]
+tp_sorted_cm = cm.iloc[tp_sorted_indices, tp_sorted_indices]
+tp_sorted_labels = [labels[i] for i in tp_sorted_indices]
+
+# %%
+# plot tp-sorted confusion matrix
+fig, ax = plt.subplots(1,1, figsize=(12,10))
+sns.heatmap(tp_sorted_cm, cmap="Blues", fmt="d", ax=ax, xticklabels=tp_sorted_labels, yticklabels=tp_sorted_labels, linewidths=0.5, linecolor="lightgrey")
+ax.set_title("TP-Sorted Confusion Matrix")
+ax.set_xlabel("Predicted")
+ax.set_ylabel("True")
