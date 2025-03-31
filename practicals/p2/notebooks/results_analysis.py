20338a3be0836cb74244800c87939448d493dce7
Zach Parent <zachparent@duck.com>
2025-03-31 12:56:41 +0200

    update formatting for confusion matrice

--- practicals/p2/notebooks/results_analysis.py
+++ practicals/p2/notebooks/results_analysis.py
@@ -27,64 +27,65 @@ confusion_matrix_dfs: dict[str, pd.DataFrame] = {}
 for path in CONFUSION_MATRICES_DIR.glob("experiment_*.csv"):
     confusion_matrix_dfs[path.stem] = pd.read_csv(path, index_col=0)
 
-
 # %%
-# normalize confusion matrix
+# plot confusion matrices
+experiment_ids = [11, 24]
+for experiment_id in experiment_ids:
+    cm = confusion_matrix_dfs[f"experiment_{experiment_id}"]
+    # normalize the predictions by the positive counts
+    positive_counts = cm.sum(axis=1)
+    cm = cm.div(positive_counts, axis=0)
 
-cm = confusion_matrix_dfs["experiment_11"]
-# normalize the predictions by the positive counts
-positive_counts = cm.sum(axis=1)
-cm = cm.div(positive_counts, axis=0)
+    # sort by the positive counts
+    sorted_indices = positive_counts.sort_values(ascending=False).index
+    positive_sorted_cm = cm.iloc[sorted_indices, sorted_indices]
+    positive_sorted_labels = [labels[i] for i in sorted_indices]
+    # sort by the true positive counts
+    tp_counts = cm.values.diagonal()
+    tp_sorted_indices = tp_counts.argsort()[::-1]
+    tp_sorted_cm = cm.iloc[tp_sorted_indices, tp_sorted_indices]
+    tp_sorted_labels = [labels[i] for i in tp_sorted_indices]
 
-# %%
-# sort by the positive counts
-sorted_indices = positive_counts.sort_values(ascending=False).index
-positive_sorted_cm = cm.iloc[sorted_indices, sorted_indices]
-positive_sorted_labels = [labels[i] for i in sorted_indices]
-# %%
-# sort by the true positive counts
-tp_counts = cm.values.diagonal()
-tp_sorted_indices = tp_counts.argsort()[::-1]
-tp_sorted_cm = cm.iloc[tp_sorted_indices, tp_sorted_indices]
-tp_sorted_labels = [labels[i] for i in tp_sorted_indices]
+    # plot both confusion matrices
+    fig, ax = plt.subplots(1, 2, figsize=(30, 14), width_ratios=[1, 1.25])
+    sns.heatmap(
+        tp_sorted_cm,
+        cmap="Blues",
+        fmt="d",
+        ax=ax[0],
+        xticklabels=tp_sorted_labels,
+        yticklabels=tp_sorted_labels,
+        linewidths=0.5,
+        linecolor="lightgrey",
+        cbar=False,
+    )
+    ax[0].set_xticklabels(ax[0].get_xticklabels(), fontsize=12)
+    ax[0].set_yticklabels(ax[0].get_yticklabels(), fontsize=12)
+    ax[0].set_title("Sorted by True Positives", fontsize=14, weight="bold")
+    ax[0].set_xlabel("Predicted", fontsize=12, weight="bold")
+    ax[0].set_ylabel("True", fontsize=12, weight="bold")
 
-# %%
-# plot both confusion matrices
-fig, ax = plt.subplots(1, 2, figsize=(30, 14), width_ratios=[1, 1.25])
-sns.heatmap(
-    tp_sorted_cm,
-    cmap="Blues",
-    fmt="d",
-    ax=ax[0],
-    xticklabels=tp_sorted_labels,
-    yticklabels=tp_sorted_labels,
-    linewidths=0.5,
-    linecolor="lightgrey",
-    cbar=False,
-)
-ax[0].set_title("True Positive-Sorted Confusion Matrix")
-ax[0].set_xlabel("Predicted")
-ax[0].set_ylabel("True")
+    sns.heatmap(
+        positive_sorted_cm,
+        cmap="Blues",
+        fmt="d",
+        ax=ax[1],
+        xticklabels=positive_sorted_labels,
+        yticklabels=positive_sorted_labels,
+        linewidths=0.5,
+        linecolor="lightgrey",
+    )
+    ax[1].set_xticklabels(ax[1].get_xticklabels(), fontsize=12)
+    ax[1].set_yticklabels(ax[1].get_yticklabels(), fontsize=12)
+    ax[1].set_title("Sorted by Ground Truth Positives", fontsize=14, weight="bold")
+    ax[1].set_xlabel("Predicted", fontsize=12, weight="bold")
+    ax[1].set_ylabel("True", fontsize=12, weight="bold")
 
-sns.heatmap(
-    positive_sorted_cm,
-    cmap="Blues",
-    fmt="d",
-    ax=ax[1],
-    xticklabels=positive_sorted_labels,
-    yticklabels=positive_sorted_labels,
-    linewidths=0.5,
-    linecolor="lightgrey",
-)
-ax[1].set_title("Positive-Sorted Confusion Matrix")
-ax[1].set_xlabel("Predicted")
-ax[1].set_ylabel("True")
+    plt.suptitle("Confusion Matrices, Normalized by Ground Truth Positives", fontsize=20, weight="bold")
+    plt.tight_layout()
 
-plt.suptitle("Confusion Matrices, normalized by positive counts", fontsize=20)
-plt.tight_layout()
-
-plt.savefig(FIGURES_DIR / "confusion_matrices_experiment_11.png", dpi=300)
-plt.show()
+    plt.savefig(FIGURES_DIR / f"confusion_matrices_experiment_{experiment_id}.png", dpi=300)
+    plt.show()
 
 # %%
 # plot the metrics
