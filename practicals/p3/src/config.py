f3c485e394ab8bb7518e726ba59a0bab47e0fc72
Zach Parent <zachparent@duck.com>
2025-05-13 10:20:38 +0200

    add setup_dataset.py and config.py

---
+++ practicals/p3/src/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Directories
+PROJECT_DIR = Path(__file__).parent.parent
+DATA_DIR = PROJECT_DIR / "data"
+RAW_DATA_ZIP_PATH = DATA_DIR / "cloth3d++_subset.zip"
+RAW_DATA_DIR = DATA_DIR / "cloth3d++_subset"
+REPORT_DIR = PROJECT_DIR / "report"
+FIGURES_DIR = REPORT_DIR / "figures"
