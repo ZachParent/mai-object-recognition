d6e41a8909ed4c3ee240f7bc920c9b938b214027
Zach Parent <zachparent@duck.com>
2025-05-22 16:29:34 +0200

    set up main to run chunks of VIT run set

--- practicals/p3/src/main.py
+++ practicals/p3/src/main.py
@@ -1,7 +1,11 @@
-from src.run_configs import PERCEPTUAL_LOSS_RUN_SET
+from src.run_configs import VIT_RUN_SET, RunSet
 from src.trainer import run_experiment
 
-RUN_SET = PERCEPTUAL_LOSS_RUN_SET
+group_id = 0
+RUN_SET = RunSet(
+    title=VIT_RUN_SET.title,
+    configs=VIT_RUN_SET.configs[group_id * 3 : (group_id + 1) * 3],
+)
 
 if __name__ == "__main__":
     width = 100
