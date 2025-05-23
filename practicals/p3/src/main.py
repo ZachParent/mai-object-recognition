c8c3d2161c2f99b87038961e08c6c6b3698314dc
Zach Parent <zachparent@duck.com>
2025-05-23 15:40:59 +0200

    run TransUNet with perceptual loss and with SMPL

--- practicals/p3/src/main.py
+++ practicals/p3/src/main.py
@@ -1,10 +1,10 @@
-from src.run_configs import VIT_RUN_SET, RunSet
+from src.run_configs import SMPL_RUN_SET, RunSet
 from src.trainer import run_experiment
 
 group_id = 0
 RUN_SET = RunSet(
-    title=VIT_RUN_SET.title,
-    configs=VIT_RUN_SET.configs[group_id * 3 : (group_id + 1) * 3],
+    title=SMPL_RUN_SET.title,
+    configs=SMPL_RUN_SET.configs[group_id * 3 : (group_id + 1) * 3],
 )
 
 if __name__ == "__main__":
